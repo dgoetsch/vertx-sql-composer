6049e7ea606794104ea73be4abb8d9610ad90c9e
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2018-03-17 23:51:54 -0500

    move repo

--- example/src/main/kotlin/dev/yn/playground/user/extensions/transaction/TransactionExtensions.kt
+++
@@ -1,9 +0,0 @@
-package dev.yn.playground.user.extensions.transaction
-
-import dev.yn.playground.sql.task.SQLClientProvider
-import dev.yn.playground.sql.task.UnpreparedSQLTask
-import dev.yn.playground.user.sql.UserTransactions
-import io.vertx.core.Future
-
-fun <P: SQLClientProvider> P.deleteAllUsers() = UnpreparedSQLTask<Unit, Unit, P>(UserTransactions.deleteAllUserActionChain()).prepare(this).run(Unit)
-fun <T, P: SQLClientProvider> Future<T>.deleteAllUsers(provider: P) = provider.deleteAllUsers()
